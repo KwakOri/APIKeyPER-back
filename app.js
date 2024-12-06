7e492cf438d3131bdf7b6d18ef0a17dffab06e71
kwakjaehun <jh5414092@icloud.com>
2024-12-07 00:05:26 +0900

    Feat: install cors module

--- app.js
+++ app.js
@@ -8,6 +8,21 @@ const bodyParser = require("body-parser");
 const nodeCron = require("node-cron");
 const { sendScheduledNotification } = require("./src/controllers/firebase");
 const logger = require("./src/config/logger");
+const cors = require("cors");
+const whitelist = ["http://localhost:3001"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      // 만일 whitelist 배열에 origin인자가 있을 경우
+      callback(null, true); // cors 허용
+    } else {
+      callback(new Error("Not Allowed Origin!")); // cors 비허용
+    }
+  },
+};
+
+app.use(cors(corsOptions)); // 옵션을 추가한 CORS 미들웨어 추가
 
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
