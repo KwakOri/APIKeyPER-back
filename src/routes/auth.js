124dd695da1593ccd678b16f229fc109e9421e87
kwakjaehun <jh5414092@icloud.com>
2024-12-10 12:17:41 +0900

    Feat: email verification code 전송 기능  구현

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const { logIn, signUp, logOut } = require("../controllers/auth");
+const { logIn, signUp, logOut, verifyEmail } = require("../controllers/auth");
 
 router.route("/log-in").post(logIn);
 router.route("/sign-up").post(signUp);
 router.route("/log-out").delete(logOut);
+router.route("/sign-up/validation/email").post(verifyEmail);
+
+// TODO:
+// auth/sign-up/validation/email
+// auth/sign-up/validation/code
 
 module.exports = router;
