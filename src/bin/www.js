28c172a309c344c2ed584cd6abc055a8612c785b
kwakjaehun <jh5414092@icloud.com>
2024-12-15 15:55:12 +0900

    Feat: app listen 수정

--- src/bin/www.js
+++ src/bin/www.js
@@ -5,6 +5,6 @@ const logger = require("../config/logger");
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   logger.info(`SERVER IS LISTENING, PORT:${PORT}`);
 });
